6db4265c2ca5fececd70befa13d39c87b2fe328a
phosphor-1 <isaacpaintball@gmail.com>
2022-08-15 12:52:16 -0700

    .

--- badass-clippy/index.php
+++ badass-clippy/index.php
@@ -6,42 +6,63 @@ require_once('helper.php');
 <!DOCTYPE html>
 <!--Epic badass skull memes with da clippy fo today-->
 <html>
+	
 	<header>
+		
 		<center>
+			
 			<h1>Welcome to BadassNet</h1>
+			
 			<h2>The World's Most Badass Free Internet</h2>
+			
 			<form action="pwned.html">
+				
 				<input type="submit" value="Click Here to Connect!" />
+			
 			</form>
+		
 		</center>
+	
 	</header>
+	
 	<body>
+				
+		<center>
+			
+			<figure>
+				
+				<img src="images/skull1.png" style="width:500px;height:500px">
+			
+			</figure>
+    
+			<figure>
+				
+				<img src="images/skull2.png" style="width:500px;height:500px">
+			
+			</figure>
+    
+			<figure>
+				
+				<img src="images/skull3.jpg" style="width:500px;height:500px">
+			
+			</figure>
+    
+			<figure>
+				
+				<img src="images/skull4.jpg" style="width:500px;height:500px">
+			
+			</figure>
+		
+		</center>
+	
+	</body>
+
 		<audio control loop autoplay src="audio/bad-to-the-bone.mp3" id="bttb"></audio>
-		
-		
+				
 		<script type="text/javascript">	
 			window.onclick=function(){
     			document.getElementById("bttb").play();
 			}	
 		</script>
-		
-		
-		<center>
-			<figure>
-				<img src="images/skull1.png" style="width:500px;height:500px">
-			</figure>
-    
-			<figure>
-				<img src="images/skull2.png" style="width:500px;height:500px">
-			</figure>
-    
-			<figure>
-				<img src="images/skull3.jpg" style="width:500px;height:500px">
-			</figure>
-    
-			<figure>
-				<img src="images/skull4.jpg" style="width:500px;height:500px">
-			</figure>
-		</center>
-	</body>
+
 </html>
